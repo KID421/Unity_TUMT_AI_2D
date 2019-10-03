dde83e61cda044d544d55d59a2c8b8d32e68ad5c
KID <50666207+KID421@users.noreply.github.com>
2019-10-03 12:07:22 +0800

    NPC 欄位設定

---
+++ 2D/Assets/Scripts/NPC.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("對話")]
+    public string sayStart = "嗨，你好，我可以請你幫我蒐集十顆櫻桃嗎？";
+    public string sayNotComplete = "你還沒找到十顆櫻桃喔...";
+    public string sayComplete = "感謝你幫我找到十顆櫻桃~";
+    [Header("對話速度")]
+    public float speed = 1.5f;
+    [Header("任務相關")]
+    public bool complete;
+    public int countPlayer;
+    public int countFinish = 10;
+}
