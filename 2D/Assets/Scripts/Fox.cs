f5b1546a1d6653b77eb14f4d9f8de1db9843b623
KID <50666207+KID421@users.noreply.github.com>
2019-10-24 12:08:21 +0800

    角色控制：左右移動與翻面

--- 2D/Assets/Scripts/Fox.cs
+++ 2D/Assets/Scripts/Fox.cs
@@ -10,6 +10,7 @@ public class Fox : MonoBehaviour    // 類別 類別名稱
     public bool pass = false;               // 布林值 - true/false
 
     private Rigidbody2D r2d;
+    //private Transform tra;
 
     // 事件：在特定時間點會以指定頻率執行的方法
     // 開始事件：遊戲開始時執行一次
@@ -17,9 +18,18 @@ public class Fox : MonoBehaviour    // 類別 類別名稱
     {
         // 泛型 <T>
         r2d = GetComponent<Rigidbody2D>();
+        //tra = GetComponent<Transform>();
     }
 
+    // 更新事件：每秒執行約 60 次
     private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) transform.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+
+    // 固定更新事件：每禎 0.002 秒
+    private void FixedUpdate()
     {
         r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
     }
