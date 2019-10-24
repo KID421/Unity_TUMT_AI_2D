395023f4dd740cb3e1cb914264ce283d3a5793e9
KID <50666207+KID421@users.noreply.github.com>
2019-10-24 10:44:37 +0800

    添加 Rigidbody2D 控制

--- 2D/Assets/Scripts/Fox.cs
+++ 2D/Assets/Scripts/Fox.cs
@@ -9,22 +9,18 @@ public class Fox : MonoBehaviour    // 類別 類別名稱
     public string foxName = "狐狸";         // 字串
     public bool pass = false;               // 布林值 - true/false
 
+    private Rigidbody2D r2d;
+
     // 事件：在特定時間點會以指定頻率執行的方法
     // 開始事件：遊戲開始時執行一次
     private void Start()
     {
-        // API 把訊息輸出到 Unity
-        Debug.Log("哈囉，沃德~@3@");
-        Debug.Log("!!!!!!!!");
-        // Log(object message) API 說明書
-        // object 指任何類型，例如：777，1.5f，"嗨~"，true/false
-        // message 指說明這裡要放什麼，這裡指的是訊息
-        Debug.Log(777);
-        Debug.Log(true);
-        Debug.Log(1.5f);
-        // 警告訊息
-        Debug.LogWarning("警告訊息");
-        // 錯誤訊息
-        Debug.LogError("錯誤訊息");
+        // 泛型 <T>
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        r2d.AddForce(new Vector2(speed, 0));
     }
 }
