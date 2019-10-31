21b55506f401d13b86e75d1ae2ad43fa3513d62a
KID <50666207+KID421@users.noreply.github.com>
2019-10-31 10:37:49 +0800

    添加方法

--- 2D/Assets/Scripts/Fox.cs
+++ 2D/Assets/Scripts/Fox.cs
@@ -24,13 +24,36 @@ public class Fox : MonoBehaviour    // 類別 類別名稱
     // 更新事件：每秒執行約 60 次
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D)) transform.eulerAngles = new Vector3(0, 0, 0);
-        if (Input.GetKeyDown(KeyCode.A)) transform.eulerAngles = new Vector3(0, 180, 0);
+        if (Input.GetKeyDown(KeyCode.D)) TurnRight();
+        if (Input.GetKeyDown(KeyCode.A)) TurnLeft();
     }
 
     // 固定更新事件：每禎 0.002 秒
     private void FixedUpdate()
     {
+        Walk(); // 呼叫方法
+    }
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
         r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
     }
+    /// <summary>
+    /// 右轉
+    /// </summary>
+    private void TurnRight()
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+    /// <summary>
+    /// 左轉
+    /// </summary>
+    private void TurnLeft()
+    {
+        transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+
 }
